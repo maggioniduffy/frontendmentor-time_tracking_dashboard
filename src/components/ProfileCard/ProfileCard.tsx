e0be3097ffd3739022facfe84899b1680f539fea
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-03-27 16:57:55 -0300

    fix responsive in fold

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { Timeframe } from "../../App";
 import './profilecard.css';
+import jeremy from '../../assets/images/image-jeremy.png';
 
 type Props = {
     timeframe: string;
@@ -11,16 +12,22 @@ const ProfileCard = ({ chooseTimeFrame, timeframe } : Props) => {
   return (
     <div className="outer-profile-card">
         <div className="inner-profile-card">
-            
+            <div className="profile-pic-container">
+                <img src={jeremy} alt='Jeremy' className='profile-pic'/>
+            </div>
+            <div className="names">
+                <h5 className="light-font"> Report for </h5>
+                <h3 className="light-font"> Jeremy Robson </h3>
+            </div>
         </div>
         <div className='options'>
-            <button onClick={() => chooseTimeFrame('daily')}>
+            <button className={timeframe === 'daily' ? 'focused' : ''} onClick={() => chooseTimeFrame('daily')}>
                 Daily
             </button>
-            <button onClick={() => chooseTimeFrame('weekly')}>
+            <button className={timeframe === 'weekly' ? 'focused' : ''} onClick={() => chooseTimeFrame('weekly')}>
                 Weekly
             </button>
-            <button onClick={() => chooseTimeFrame('monthly')}>
+            <button className={timeframe === 'monthly' ? 'focused' : ''} onClick={() => chooseTimeFrame('monthly')}>
                 Monthly
             </button>
         </div>
